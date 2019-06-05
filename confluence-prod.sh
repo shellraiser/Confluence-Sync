89ddc3aad986a5d3b5c0f90c361a8e074f85f3b6
Paige Scheller <paige.m.scheller@gmail.com>
2019-06-05 18:37:16 -0500

    Update confluence-prod.sh

--- confluence-prod.sh
+++ confluence-prod.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #Create necessary directories if they don't exist
-mkdir -p /opt/confluence_tasks/BACKUP/conf_install/
-mkdir -p /opt/confluence_tasks/BACKUP/conf_home/
-mkdir -p /opt/confluence_tasks/BACKUP/conf_sql/
+mkdir -p /opt/confluence-sync/BACKUP/conf_install/
+mkdir -p /opt/confluence-sync/BACKUP/conf_home/
+mkdir -p /opt/confluence-sync/BACKUP/conf_sql/
 
 #Parameters for directories
 conf_install=/opt/atlassian/confluence
 conf_home=/var/atlassian/application-data/confluence
-conf_install_backup=/opt/confluence_tasks/BACKUP/conf_install/`date +%Y%m%d-%H%M%S`
-conf_home_backup=/opt/confluence_tasks/BACKUP/conf_home/`date +%Y%m%d-%H%M%S`
-conf_sql_backup=/opt/confluence_tasks/BACKUP/conf_sql/`date +%Y%m%d-%H%M%S`
+conf_install_backup=/opt/confluence-sync/BACKUP/conf_install/`date +%Y%m%d-%H%M%S`
+conf_home_backup=/opt/confluence-sync/BACKUP/conf_home/`date +%Y%m%d-%H%M%S`
+conf_sql_backup=/opt/confluence-sync/BACKUP/conf_sql/`date +%Y%m%d-%H%M%S`
 
 #Create the dated subdirectories
 mkdir $conf_install_backup
@@ -18,19 +18,19 @@ mkdir $conf_home_backup
 mkdir $conf_sql_backup
 
 #Delete older directories
-sudo find /opt/confluence_tasks/BACKUP/conf_install/ -type d -ctime +1 -exec rm -rf {} \;
-sudo find /opt/confluence_tasks/BACKUP/conf_home/ -type d -ctime +1 -exec rm -rf {} \;
-sudo find /opt/confluence_tasks/BACKUP/conf_sql/ -type d -ctime +1 -exec rm -rf {} \;
+sudo find /opt/confluence-sync/BACKUP/conf_install/ -type d -ctime +1 -exec rm -rf {} \;
+sudo find /opt/confluence-sync/BACKUP/conf_home/ -type d -ctime +1 -exec rm -rf {} \;
+sudo find /opt/confluence-sync/BACKUP/conf_sql/ -type d -ctime +1 -exec rm -rf {} \;
 
 #Point backup directories to new folders
-conf_install_backup=$(ls -td /opt/confluence_tasks/BACKUP/conf_install/* | head -1)
-conf_home_backup=$(ls -td /opt/confluence_tasks/BACKUP/conf_home/* | head -1)
-conf_sql_backup=$(ls -td /opt/confluence_tasks/BACKUP/conf_sql/* | head -1)
+conf_install_backup=$(ls -td /opt/confluence-sync/BACKUP/conf_install/* | head -1)
+conf_home_backup=$(ls -td /opt/confluence-sync/BACKUP/conf_home/* | head -1)
+conf_sql_backup=$(ls -td /opt/confluence-sync/BACKUP/conf_sql/* | head -1)
 
 #Stop Confluence service
 sudo /opt/atlassian/confluence/bin/stop-confluence.sh
 
-#Perform mySQL dump to backup location. Enter mySQL pw here
+#Perform mySQL dump to backup location. Enter your mySQL pw here
 mysqldump --user=confluenceuser --password=password --lock-tables --databases confluence > $conf_sql_backup/confluence.sql
 
 #Copy data from install and home directories to backup locations
@@ -40,7 +40,8 @@ sudo rsync -av --exclude=backups $conf_install $conf_install_backup
 #Start Confluence service
 sudo /opt/atlassian/confluence/bin/start-confluence.sh
 
-#Copy data from PROD to DEV/STAGING. Enter in your own environment values here. Make sure the folders already exist on your staging/dev server. Setup an SSL key to do this, so no password is needed.
-sudo rsync -av $conf_home_backup user@confluence-dev.pawsch.net:/opt/confluence_tasks/UPDATE/conf_home/
-sudo rsync -av $conf_install_backup user@confluence-dev.pawsch.net:/opt/confluence_tasks/UPDATE/conf_install/
-sudo rsync -av $conf_sql_backup user@confluence-dev.pawsch.net:/opt/confluence_tasks/UPDATE/conf_sql/
+#Copy data from PROD to DEV/STAGING. Enter in your own environment values here. Make sure the folders already 
+#exist on your staging/dev server. Setup an SSL key to do this, so no password is needed.
+sudo rsync -av $conf_home_backup user@confluence-dev.pawsch.net:/opt/confluence-sync/UPDATE/conf_home/
+sudo rsync -av $conf_install_backup user@confluence-dev.pawsch.net:/opt/confluence-sync/UPDATE/conf_install/
+sudo rsync -av $conf_sql_backup user@confluence-dev.pawsch.net:/opt/confluence-sync/UPDATE/conf_sql/
